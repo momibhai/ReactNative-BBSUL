0317c36dd09bbd5edbcc925ec838da97a5ba3485
Ali <rayangosling795@gmail.com>
2023-06-20 22:55:50 +0500

    Second commit

--- App.tsx
+++ App.tsx
@@ -3,16 +3,16 @@ import React from 'react'
 
   const array =[
     {name:'Ali',email:'Ali@gmail.com',address:'Malir',image:require('./images/messages2.jpg')}
-    ,{name:'Muhammad ',email:'Muha@gmail.com',address:'Lyari',image:require('./images/messages-3.jpg')}
-    ,{name:'Karachi',email:'Karachi@gmail.com',address:'Karachi',image:require('./images/messages-1.jpg')}
-    ,{name:'Ali',email:'Ali@gmail.com',address:'MalirKarachi',image:require('./images/content-5.png')},
-    {name:'Janjan',email:'Janjan@gmail.com',address:'Karai',image:require('./images/profile-img.jpg')},
-    {name:'Janjan',email:'Janjan@gmail.com',address:'Karai',image:require('./images/content-5.png')}
+    ,{name:'Momi',email:'Muha@gmail.com',address:'Lyari',image:require('./images/messages-3.jpg')}
+    ,{name:'Aslam',email:'Kara@gmail.com',address:'Karachi',image:require('./images/messages-1.jpg')}
+    ,{name:'Ali',email:'Ali@gmail.com',address:'MalirKarachi',image:require('./images/profile-img.jpg')},
+    {name:'umair',email:'umair@gmail.com',address:'Karai',image:require('./images/news-2.jpg')},
+    {name:'umair',email:'umair@gmail.com',address:'Karai',image:require('./images/news-3.jpg')}
   ]
 const App = () => {
   return (
     <View style={styles.container}>
-        <Text style={styles.mainheadertext}>WelCome To My FlateList</Text>
+        <Text style={styles.mainheadertext}>Welcome To My Profile FlateList</Text>
         <FlatList 
           data={array}
           renderItem={({item})=>
@@ -26,7 +26,7 @@ const App = () => {
               <Text style={{marginLeft:10,marginTop:10}}>
                 Address : {item.address}
               </Text>
-              <View style={{height:100,width:100,borderRadius:50,marginLeft:160,marginTop:-100,backgroundColor:'#d6d6d6'}}>
+              <View style={{height:100,width:100,borderRadius:50,marginLeft:160,marginTop:-100,}}>
             <Image 
             style={styles.image}
             resizeMode='contain'
